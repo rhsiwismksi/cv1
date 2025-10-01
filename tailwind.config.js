1c3c2f91589a0c3c17110582f4c1e5edfe87de71
anh627 <linhanhtung08@gmail.com>
2025-10-01 08:00:02 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'xs': '475px',
+      },
+      animation: {
+        'place-stone': 'place-stone 0.3s ease-out',
+      },
+    },
+  },
+  plugins: [],
+}
